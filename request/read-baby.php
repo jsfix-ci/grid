0c8e7af01de7a2e3ac3c2a05cff2499f2c392c1c
mwyatt <martin.wyatt@gmail.com>
2016-01-23 08:30:37 +0000

    begun to implement framework for crud

--- request/read-baby.php
+++ request/read-baby.php
@@ -1,13 +1,24 @@
 <?php
 
-echo json_encode((object) [
-	'rows' => [
-		[1, 2, 3],
+// here get doctrine instance and create query based on the request given then return rows, page number, limit
+
+// echo '<pre>';
+// print_r($_REQUEST);
+// echo '</pre>';
+// exit;
+
+$rows = [
 		[1, 2, 3],
 		[1, 2, 3],
 		[1, 2, 3],
 		[1, 2, 3],
 		[1, 2, 3],
 		[1, 2, 3]
-	]
+	];
+
+echo json_encode((object) [
+	'rowsTotal' => count($rows), 
+	'pageCurrent' => 1,
+	'rowsPerPage' => 10,
+	'rows' => $rows
 ]);
